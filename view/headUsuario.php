966f365dc3a4d0383832318e81c749548d843c70
AlejandraOlayaPerez <83318494+AlejandraOlayaPerez@users.noreply.github.com>
2021-07-02 15:48:03 -0500

    FECHA: 02/07/2021
    Subo la carpeta de asset, la cual dividí en dos para poder ssubirla.

---
+++ view/headUsuario.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="/Anyeale_proyecto/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="/Anyeale_proyecto/css/all.min.css" rel="stylesheet">
+    <link rel="/anyeale_proyecto/css/estilosAnyeale.css">
+    <script src="/Anyeale_proyecto/js/bootstrap.min.js"></script>
+    <script src="/Anyeale_proyecto/js/popper.min.js"></script>
+    <script src="/Anyeale_proyecto/js/jquery-3.6.0.min.js"></script>
+    <script src="/Anyeale_proyecto/js/validacion.js"></script>
+    <link rel="shortcut icon" href="/Anyeale_proyecto/img/PNG LOGO.png" type="image/x-icon">
+    <title>STYLUSH ANYEALE</title>
+</head>
+
+<body style="background-color:rgba(255,255,204,255); margin-top:2px;">
+    <div class="container">
+        <div style="background-color:rgb(119,167,191); height:auto">
+            <button type="button" style="background-color:rgb(119,167,191); border: 2px solid rgb(249,201,242); font-family:'Times New Roman', Times, serif; font-weight: bold; font-size:25px; position:relative; left:690px;"><i class="fas fa-sign-in-alt"></i> Iniciar sesion</button>
+            <button type="button" style="background-color:rgb(119,167,191); border: 2px solid rgb(249,201,242); font-family:'Times New Roman', Times, serif; font-weight: bold; font-size:25px; position:relative; left:690px;"><i class="fas fa-book-open"></i> Registrarse</button>
+            <img src="/Anyeale_proyecto/img/PNG LOGO.png" style="width:220px; height:auto; position:relative; top:15px; right: 355px;">
+            <h1 style="font-family:'Times New Roman', Times, serif; -webkit-text-stroke: 2px rgb(249,201,242); -webkit-text-fill-color: rgb(119,167,191); font-weigh; font-size:90px; position:relative; left:260px; bottom: 80px;">STYLUSH ANYEALE</h1>
+        </div>
+        <div>
+            <nav class="navbar navbar-expand-lg" style="background-color: rgb(249,201,242); position:relative; bottom:60px; height:auto;">
+                <div class="container-fluid" style="background-color: rgb(249,201,242);">
+                    <strong><a style="margin-right:5px; margin-left:5px; background-color:rgb(249,201,242); font-family:'Times New Roman', Times, serif; font-weight: bold; font-size:30px; color:black;" class="nav-link active" href=""><i class="fas fa-home"></i> STYLUSH ANYEALE</a></strong>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><strong><a style="margin-right:2px; margin-left:2px; background-color:rgb(249,201,242); font-family:'Times New Roman', Times, serif; font-weight: bold; font-size:30px; color:black;" class="nav-link active" aria-current="page" href=""><i class="fas fa-image"></i> Galeria</a></strong></li>
+                        <li class="nav-item"><strong><a style="margin-right:2px; margin-left:2px; background-color:rgb(249,201,242); font-family:'Times New Roman', Times, serif; font-weight: bold; font-size:30px; color:black;" class="nav-link active" aria-current="page" href=""><i class="fas fa-wine-bottle"></i> Producto</a></strong></li>
+                        <li class="nav-item"><strong><a style="margin-right:2px; margin-left:2px; background-color:rgb(249,201,242); font-family:'Times New Roman', Times, serif; font-weight: bold; font-size:30px; color:black;" class="nav-link active" aria-current="page" href=""><i class="fas fa-cut"></i> Servicios</a></strong></li>
+                        <li class="nav-item"><strong><a style="margin-right:2px; margin-left:2px; background-color:rgb(249,201,242); font-family:'Times New Roman', Times, serif; font-weight: bold; font-size:30px; color:black;" class="nav-link active" aria-current="page" href=""><i class="fas fa-clock"></i> Reserva</a></strong></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+</body>
+
+</html>
